dbe506ff9a2caee59e185106ecb51b24fc2bac1c
Matthew Aquiles <matt@peeriq.com>
2021-12-20 21:23:54 -0500

    remove unused imports

--- footy-picks-fe/src/pages/dashboard.js
+++ footy-picks-fe/src/pages/dashboard.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, Col, Container, Row } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import LiveScores from "../components/LiveScores";
 import { ActiveGames } from "../components/ActiveGames";
 
