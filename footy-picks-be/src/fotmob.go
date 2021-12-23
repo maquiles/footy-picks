815d59cb53a019ece1e406a080a1af72a7843ebb
Matthew Aquiles <matt@peeriq.com>
2021-12-22 22:06:59 -0500

    fotmob api scraping updates

--- footy-picks-be/src/fotmob.go
+++ footy-picks-be/src/fotmob.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
 )
 
+// FOTMOB URLS
 var FOTMOB_BASE_URL string = "https://www.fotmob.com"
 var MATCHES_URL string = FOTMOB_BASE_URL + "/matches?"
 var LEAGUES_URL string = FOTMOB_BASE_URL + "/leagues?"
@@ -15,8 +17,86 @@ var PLAYERS_URL string = FOTMOB_BASE_URL + "/platerData?"
 var MATCH_DETAILS_URL string = FOTMOB_BASE_URL + "/matchDetails?"
 var SEARCH_URL = FOTMOB_BASE_URL + "/searchapi/"
 
-func getLeague(id string, tab string, typ string, timeZone string) string {
-	url := fmt.Sprintf("%sid=%s&tab=%s&type=%s&timeZone=%s", LEAGUES_URL, id, tab, typ, timeZone)
+//FOTMOB LEAGUE IDS
+var LEAGUE_IDS map[string]string = map[string]string{
+	"UCL":  "42",
+	"EPL":  "47",
+	"EURO": "50",
+}
+
+//FOTMOB MODELS
+type Team struct {
+	ID        string `json:"id"`
+	Name      string `json:"name"`
+	ShortName string `json:"shortName"`
+}
+
+type MatchFinishedReason struct {
+	Long  string `json:"long"`
+	Short string `json:"short"`
+}
+
+type MatchStatus struct {
+	Cancelled         bool                `json:"cancelled"`
+	Finished          bool                `json:"finished"`
+	Reason            MatchFinishedReason `json:"reason"`
+	ScoreStr          string              `json:"scoreStr"`
+	StartDateStr      string              `json:"startDateStr"`
+	StartDateStrShort string              `json:"startDateStrShort"`
+	Started           bool                `json:"started"`
+}
+
+type Match struct {
+	Away      Team        `json:"away"`
+	Home      Team        `json:"home"`
+	ID        string      `json:"id"`
+	MonthKey  string      `json:"monthKey"`
+	PageURL   string      `json:"pageUrl"`
+	Round     int         `json:"round"`
+	RoundName int         `json:"roundName"`
+	Status    MatchStatus `json:"status"`
+}
+
+type LeagueDetails struct {
+	Country   string      `json:"country"`
+	FaqJSONLD interface{} `json:"faqJSONLD"`
+	ID        int         `json:"id"`
+	Name      string      `json:"name"`
+	ShortName string      `json:"shortName"`
+	Type      string      `json:"typw"`
+}
+
+type FirstUnplayedMatch struct {
+	FirstRoundWithUnplayedMatch int `json:"firstRoundWithUnplayedMatch"`
+	FirstUnplayedMatchIndex     int `json:"firstUnplayedMatchIndex"`
+}
+
+type MatchesTabData struct {
+	AllMatches             []Match     `json:"allMatches"`
+	MatchesCombinedByRound interface{} `json:"matchesCombinedByRound"`
+}
+
+type MatchesTab struct {
+	Data               MatchesTabData     `json:"data"`
+	FirstUnplayedMatch FirstUnplayedMatch `json:"firstUnplayedMatch"`
+	Seostr             string             `json:"seostr"`
+	Tab                string             `json:"tab"`
+}
+
+type LeagueResponse struct {
+	QAData     interface{}   `json:"QAData"`
+	Action     interface{}   `json:"action"`
+	Datasets   interface{}   `json:"datasets"`
+	Details    LeagueDetails `json:"details"`
+	MatchesTab MatchesTab    `json:"matchesTab"`
+	Seostr     string        `json:"seostr"`
+	Tab        string        `json:"tab"`
+	Tabs       []string      `json:"tabs"`
+}
+
+func GetLeagueMatches(league string, tab string, timezone string) []Match {
+	leaugeID := LEAGUE_IDS[league]
+	url := fmt.Sprintf("%stimezone=%s&id=%s&tab=%s", LEAGUES_URL, timezone, leaugeID, tab)
 	log.Printf("sending fotmob request %s", url)
 
 	response, err := http.Get(url)
@@ -29,5 +109,10 @@ func getLeague(id string, tab string, typ string, timeZone string) string {
 		log.Println("error parsing response body from getLeague >>", err)
 	}
 
-	return string(body)
+	var leagueResponse LeagueResponse
+	json.Unmarshal(body, &leagueResponse)
+
+	matches := leagueResponse.MatchesTab.Data.AllMatches
+
+	return matches
 }
--- footy-picks-be/src/main.go
+++ footy-picks-be/src/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 
@@ -20,13 +21,33 @@ func Init() *App {
 	return app
 }
 
+// ROUTE HANDLERS
+func (app *App) HealthHandler(writer http.ResponseWriter, request *http.Request) {
+	log.Println("received /health request")
+	json.NewEncoder(writer).Encode("literally just vibin")
+}
+
+func (app *App) MatchDataRefreshHandler(writer http.ResponseWriter, request *http.Request) {
+	league := request.FormValue("league")
+	tab := request.FormValue("tab")
+	timeZone := request.FormValue("timezone")
+
+	response := GetLeagueMatches(league, tab, timeZone)
+	json.NewEncoder(writer).Encode(response)
+}
+
+// ROUTES
 func (app *App) initRoutes() {
 	// health
 	app.Router.HandleFunc("/health", app.HealthHandler).Methods("GET")
 
 	// get fotmob data
-	app.Router.HandleFunc("/match-data/refresh", app.MatchDataRefreshHandler).Methods("GET")
+	app.Router.HandleFunc("/match-data/refresh", app.MatchDataRefreshHandler).Methods("GET").Queries(
+		"league", "{league}",
+		"tab", "{tab}",
+		"timezone", "{timezone}")
 }
+
 func (a *App) Run(addr string) {
 	log.Println("Starting Server at", addr)
 
@@ -39,6 +60,7 @@ func (a *App) Run(addr string) {
 	log.Fatal(http.ListenAndServe(addr, handler))
 }
 
+// MAIN
 func main() {
 	app := Init()
 	app.Run("0.0.0.0:8000")
