21be34189d13110030bd5d0dea14a701ad838dca
Matthew Aquiles <matt@peeriq.com>
2021-12-28 18:15:51 -0500

    mock data in db script

--- footy-picks-be/db/create_tables.sql
+++ footy-picks-be/db/create_tables.sql
@@ -27,3 +27,32 @@ CREATE TABLE survivor_pick (
   player integer REFERENCES player,
   PRIMARY KEY (game, game_round, player)
 );
+
+/* TODO: Remove mock data */
+/* players */
+INSERT INTO player (player_id, email, player_name, player_login, created, games) 
+  VALUES (1, 'mattaquiles@gmail.com', 'Matthew', 'fakelogin', '2021-12-28', '{1,2}');
+INSERT INTO player (player_id, email, player_name, player_login, created, games) 
+  VALUES (2, 'player2@gmail.com', 'Player 2', 'fakelogin', '2021-12-28', '{1,2}');
+INSERT INTO player (player_id, email, player_name, player_login, created, games) 
+  VALUES (3, 'plqyer3@gmail.com', 'Player 3', 'fakelogin', '2021-12-28', '{1,2}');
+INSERT INTO player (player_id, email, player_name, player_login, created, games) 
+  VALUES (4, 'player4@gmail.com', 'Player 4', 'fakelogin', '2021-12-28', '{1,2}');
+INSERT INTO player (player_id, email, player_name, player_login, created, games) 
+  VALUES (5, 'player5@gmail.com', 'Player 5', 'fakelogin', '2021-12-28', '{1,2}');
+INSERT INTO player (player_id, email, player_name, player_login, created, games) 
+  VALUES (6, 'player6@gmail.com', 'Player 6', 'fakelogin', '2021-12-28', '{1,2}');
+INSERT INTO player (player_id, email, player_name, player_login, created, games) 
+  VALUES (7, 'player7@gmail.com', 'Player 7', 'fakelogin', '2021-12-28', '{1,2}');
+INSERT INTO player (player_id, email, player_name, player_login, created, games) 
+  VALUES (8, 'player8@gmail.com', 'Player 8', 'fakelogin', '2021-12-28', '{1,2}');
+INSERT INTO player (player_id, email, player_name, player_login, created, games) 
+  VALUES (9, 'player9@gmail.com', 'Player 9', 'fakelogin', '2021-12-28', '{1,2}');
+INSERT INTO player (player_id, email, player_name, player_login, created, games) 
+  VALUES (10, 'player10@gmail.com', 'Player 10', 'fakelogin', '2021-12-28', '{1,2}');
+
+/* survivor_games */
+
+
+
+/* survivor_picks */
